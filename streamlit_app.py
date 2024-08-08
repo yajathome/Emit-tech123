12bb8f4069b5b1d30d1924e9468b041fa1df4654
yajathome <119949745+yajathome@users.noreply.github.com>
2024-08-08 13:12:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+a="KA 06 1234"
+b="72.5"
+st.html("<h1 style='font-size:100px;text-align:center;background-color:#04336b;border-radius:20px;'>Emi-tech</h1>")
+st.subheader("Number Plate")
+st.write(a)
+st.divider()
+st.subheader("Aqi level")
+st.write(b)
+st.markdown('''<style>
+                .st-emotion-cache-1v1dp7g p {
+                    word-break: break-word;
+                    font-size: 90px;
+                    }
+                .st-emotion-cache-1v1dp7g p {
+                    word-break: break-word;
+                    font-size: 90px;
+                    }
+            </style>''',unsafe_allow_html=True)
