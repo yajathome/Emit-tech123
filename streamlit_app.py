eae8b28882cd84af2949cd96d289f5a033541cc0
yajathome <119949745+yajathome@users.noreply.github.com>
2024-08-08 18:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-a="KA 06 1234"
-b="72.5"
+import cv2
 st.set_page_config(layout="wide")
 st.html("<h1 style='font-size:100px;text-align:center;background-color:#04336b;border-radius:20px;'>Emi-tech</h1>")
 st.subheader("Number Plate")
-st.write(a)
+st.write("ka 60 1234")
 st.divider()
 st.subheader("Aqi level")
-st.write(b)
+st.write(72.5)
 st.markdown('''<style>
                 .st-emotion-cache-1sno8jx p{
                     word-break: break-word;
@@ -18,3 +17,17 @@ st.markdown('''<style>
                     font-size: 90px;
                     }
             </style>''',unsafe_allow_html=True)
+a=st.button("picture")
+if a:
+  cam = cv2.VideoCapture(0)
+  if not cam.isOpened():
+      print("Error: Could not open camera.")
+      exit()
+  result, image = cam.read()
+  if result:
+      cv2.imshow("GeeksForGeeks", image)
+      cv2.imwrite("GeeksForGeeks.png", image)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
+  else:
+      print("No image detected. Please try again.")
